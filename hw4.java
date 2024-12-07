328b94969618e4db2a92dade9de00173f700f96f
OVvvERLORD <temy24101962@gmail.com>
2024-12-07 05:05:19 +0300

    less_shitty_all

--- hw4.java
+++ hw4.java
@@ -37,15 +37,39 @@ class union{
      int i;
 }
 class parser {
-     public union pars(int i, char [] norm_input_ch) {
+     public union pars1(int i, char [] norm_input_ch) {
           while (norm_input_ch[i] != ':') {
                     i++;
                }
           i+=2;
           int j = 0;
           int char_cnt = 0;
-          char [] temp_str = new char [50]; 
-          while (norm_input_ch[i] != '"'){
+          char [] temp_str = new char [55]; 
+          while (norm_input_ch[i] != '"' && norm_input_ch[i] != 0){
+               temp_str[j] = norm_input_ch[i];
+               i++;
+               j++;
+               char_cnt+=1;
+          }
+          char [] add_str = new char [char_cnt+1];
+          for (int r = 0; r < char_cnt; r++) {
+               add_str[r] = temp_str[r];
+          }
+          String temp_string = new String(add_str);
+          union ret = new union();
+          ret.i = i;
+          ret.str = temp_string;
+          return ret;
+     }
+     public union pars2(int i, char [] norm_input_ch) {
+          while (norm_input_ch[i] != ':' && norm_input_ch[i] != 0) {
+                    i++;
+               }
+          i++;
+          int j = 0;
+          int char_cnt = 0;
+          char [] temp_str = new char [55]; 
+          while (norm_input_ch[i] != ',' && norm_input_ch[i] != '}'){
                temp_str[j] = norm_input_ch[i];
                i++;
                j++;
@@ -92,25 +116,25 @@ class first_query{
                     user current = new user();
                     current.id = count;
                     parser parser = new parser();
-                    current.name = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.company = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.username = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.email = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.address = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.zip = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.state = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.country = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.phone = parser.pars(i, norm_input_ch).str;
-                    i = parser.pars(i, norm_input_ch).i;
-                    current.Photo = parser.pars(i, norm_input_ch).str;
+                    current.name = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.company = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.username = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.email = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.address = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.zip = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.state = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.country = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.phone = parser.pars1(i, norm_input_ch).str;
+                    i = parser.pars1(i, norm_input_ch).i;
+                    current.Photo = parser.pars1(i, norm_input_ch).str;
                     users_set.add(current);
                    }
                }
@@ -118,7 +142,7 @@ class first_query{
                     Iterator<user> it = users_set.iterator();
                     while (it.hasNext()) {
                          user curr = it.next();
-                         System.out.println(curr.id+" "+curr.name+" "+curr.company+" "+curr.username+" "+curr.email+" "+curr.address+" "+curr.zip+" "+curr.state+" "+curr.country+" "+curr.phone+" "+curr.Photo+" ");
+                         System.out.println(curr.id+" ; "+curr.name+" ; "+curr.company+" ; "+curr.username+" ; "+curr.email+" ; "+curr.address+" ; "+curr.zip+" ; "+curr.state+" ; "+curr.country+" ; "+curr.phone+" ; "+curr.Photo+" ; ");
                          System.out.print("\n");
                     }
                } catch (Exception e) {
@@ -129,10 +153,85 @@ class first_query{
           }
      }
 }
+class todo {
+     int userId;
+     int id;
+     String title;
+     boolean completed;
+}
+class second_query {
+     public void second(){
+          try {
+               URL url = new URL("https://dummy-json.mock.beeceptor.com/todos".toString());
+               URLConnection conn = url.openConnection();
+               conn.connect();
+               InputStream inp_str = conn.getInputStream();
+               InputStreamReader reader = new InputStreamReader(inp_str);
+               BufferedReader input = new BufferedReader(reader);
+               String temp;
+               String norm_input = "";
+               while ((temp = input.readLine()) != null) {
+                    norm_input += temp;
+               }
+               norm_input = norm_input.replace('[', ' ').replace(']', ' ').replaceAll("id", "").replaceAll("userId", "").replaceAll("title","").replaceAll("completed", "").replaceAll(": ", ":");
+               LinkedHashSet todos_set = new LinkedHashSet<todo>();
+               
+               char [] norm_input_ch = norm_input.toCharArray();
+               for (int i = 0; i < norm_input_ch.length; i++) {
+                   if (norm_input_ch[i] == '{') {
+                    todo current = new todo();
+                    parser parser = new parser();
+                    char [] test = parser.pars2(i, norm_input_ch).str.toCharArray();
+                    i = parser.pars2(i, norm_input_ch).i;
+                    if (test[0] == '1' && test[1] == '0') {
+                         current.userId = 10;
+                    } else {
+                         current.userId = Integer.valueOf(test[0]) - 48;
+                    }
+                    test = parser.pars2(i, norm_input_ch).str.toCharArray();
+                    i = parser.pars2(i, norm_input_ch).i;
+                    if (test[1]!=0) {
+                         char [] num = new char [2];
+                         num[0] = test[0];
+                         num[1] = test[1];
+                         String fin_num = new String(num);
+                         current.id = Integer.parseInt(fin_num);
+                    } else {
+                         current.id = Integer.valueOf(test[0]) - 48;
+                    }
+                    test = parser.pars2(i, norm_input_ch).str.toCharArray();
+                    i = parser.pars2(i, norm_input_ch).i;
+                    String finals = new String(test);
+                    current.title = finals;
+                    test = parser.pars2(i, norm_input_ch).str.toCharArray();
+                    if (test[0] == 'f') {
+                         current.completed = false;
+                    } else {
+                         current.completed = true;
+                    }
+                    todos_set.add(current);
+                   }
+               }
+               Iterator<todo> it = todos_set.iterator();
+               while (it.hasNext()) {
+                    todo curr = it.next();
+                    System.out.println(curr.userId+" ; "+curr.id+" ; "+curr.title+" ; "+curr.completed);
+                    System.out.print("\n");
+               }
+          } catch (Exception e) {
+               System.err.println(e.getMessage());
+          }
+     }
+}
 public class hw4 {
      public static void main(String[] args){
+          System.out.print("------------------------------------------------------------------------------------------\n");
           first_query part_1 = new first_query();
           part_1.first();
+          System.out.print("------------------------------------------------------------------------------------------\n");
+          second_query part_2 = new second_query();
+          part_2.second();
+          System.out.print("------------------------------------------------------------------------------------------\n");
      }
 }
 
